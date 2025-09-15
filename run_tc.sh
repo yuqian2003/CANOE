c50066ce336922ff5ce180d4b4949a8571e437f0
yuqian2003 <2212278116@qq.com>
2025-09-15 14:09:43 +0800

    Update run_tc.sh

--- run_tc.sh
+++ run_tc.sh
@@ -21,7 +21,7 @@ LR=0.05
 ENTROPY_LOW=0.40
 CHAOS_LIST=0.75,0.8,0.85,0.9
 
-echo "=== CNOLP Training Start ==="
+echo "=== CANOE Training Start ==="
 
 reset_tc
 python model/run.py \
@@ -40,7 +40,7 @@ python model/run.py \
   --entropy_low ${ENTROPY_LOW} \
   --chaotic_high_list ${CHAOS_LIST}
 
-echo "CNOLP Training Completed."
+echo "CANOE Training Completed."
 
 
 
